8ae66ea72df77b47a2bc73d538e4789b290e7692
wizz <amar.malkic96@gmail.com>
2021-09-17 00:26:45 +0200

    +ex

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo){
+    // only change code below this line
+var remainder;
+var numOne;
+var numTwo;
+remainder = numOne % numTwo;
+
+parseInt(numOne);
+parseInt(numTwo);
+    // only change code above this line
+    return remainder;
+}
+console.log(main(5, 2)) //
+console.log(main(15, 4))
+console.log(main(102, 25))
+module.exports = main;
